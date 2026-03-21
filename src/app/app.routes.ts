4bd0697266a2de3fac7fea9c2e63be1bd9cfe200
Sebastian Dcroz Cardenas <dcroz8c@gmail.com>
2026-03-21 22:20:47 +0100

    esqueleto de rutas creadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: UserListComponent },
+  { path: 'nuevo-usuario', component: UserFormComponent },
+  { path: 'usuario/:idusuario', component: UserViewComponent },
+  { path: '**', redirectTo: 'home' }
+];
