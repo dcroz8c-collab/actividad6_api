198a102e3ec1346ecb0887396cf64fa79a70960b
Sebastian Dcroz Cardenas <dcroz8c@gmail.com>
2026-03-23 20:41:52 +0100

    gestion de errores y estados de carga implementados

--- src/app/components/user-view/user-view.component.ts
+++ src/app/components/user-view/user-view.component.ts
@@ -15,22 +15,34 @@ export class UserViewComponent implements OnInit {
   private activatedRoute = inject(ActivatedRoute);
   private router = inject(Router);
 
-  public user = signal<IUser | null>(null);
+  public user = signal<IUser | undefined | null>(undefined);
 
-  ngOnInit() {
-    this.activatedRoute.params.subscribe((params) => {
-      this.usersService.getById(params['id']).subscribe((response) => {
-        this.user.set(response);
-      });
+ngOnInit() {
+  this.activatedRoute.params.subscribe((params) => {
+    const id = params['id'];
+    this.user.set(undefined);
+    this.usersService.getById(id).subscribe({
+      next: (response) => {
+        if (response && response._id) {
+          this.user.set(response);
+        } else {
+          this.user.set(null);
+        }
+      },
+      error: (err) => {
+        console.error('Error en la API:', err);
+        this.user.set(null);
+      }
     });
-  }
+  });
+}
 
   deleteUser() {
-    const u = this.user();
-    if (!u) return;
-    const confirmed = confirm(`¿Seguro que quieres eliminar a ${u.first_name} ${u.last_name}?`);
+    const user = this.user();
+    if (!user) return;
+    const confirmed = confirm(`¿Seguro que quieres eliminar a ${user.first_name} ${user.last_name}?`);
     if (confirmed) {
-      this.usersService.delete(u._id!).subscribe({
+      this.usersService.delete(user._id!).subscribe({
         next: () => this.router.navigate(['/home']),
         error: () => alert('Error al eliminar el usuario.')
       });
